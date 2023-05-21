7abf93a3054d3c921147c499dcddae65a9dade5d
Shako Rzayev <shahriyar.rzayev@nordsec.com>
2023-05-21 16:24:44 +0200

    release: version bump

--- src/readonce.py
+++ src/readonce.py
@@ -8,7 +8,7 @@ from typing import Any, Iterable, List, Optional
 import icontract
 from cryptography.fernet import Fernet
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 
 class UnsupportedOperationException(Exception):
